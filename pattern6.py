b3d75fb91c6e0ab76e0fbd780b3b6775d0a5e925
ASWATHI23 <110898037+ASWATHI23@users.noreply.github.com>
2022-08-29 15:33:26 +0530

    fifth commit

---
+++ pattern6.py
@@ -0,0 +1,16 @@
+b=0
+for i in range(5,0,-1):
+    b+=1
+    for j in range(1,i+1):
+        print(b,end="")
+    print("\r")
+print("...............")
+for i in range(5,0,-1):
+    for j in range(0,i):
+        print(5,end="")
+    print("\r")
+print("...............")
+for i in range(0,5):
+    for j in range(0,i):
+        print(5,end="")
+    print("\r")
